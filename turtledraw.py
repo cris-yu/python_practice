51ef094d56a42b8ebdad30bfd9c01b492833ae94
crisyu <1021177281@qq.com>
2021-01-01 00:37:07 +0800

    库调用方法

--- turtledraw.py
+++ turtledraw.py
@@ -1,5 +1,5 @@
 # pythondraw.py
-import turtle
+'''import turtle
 turtle.setup(650, 350, 200, 200)
 turtle.penup()
 turtle.fd(-250)
@@ -14,4 +14,21 @@ turtle.circle(40, 80/2)
 turtle.fd(40)
 turtle.circle(16, 180)
 turtle.fd(40*2/3)
-turtle.done()
+turtle.done()'''
+
+import turtle as t
+t.setup(650, 350, 200, 200)
+t.penup()
+t.fd(-250)
+t.pendown()
+t.pensize(25)
+t.pencolor("purple")
+t.seth(-40)
+for i in range(4):
+    t.circle(40, 80)
+    t.circle(-40, 80)
+t.circle(40, 80/2)
+t.fd(40)
+t.circle(16, 180)
+t.fd(40*2/3)
+t.done()
