ee62f834dc36f1c2e2bccf2bfb730fec858a6c17
crisyu <1021177281@qq.com>
2021-01-01 23:11:44 +0800

    进度条修改

--- textprobar.py
+++ textprobar.py
@@ -1,11 +1,11 @@
 # 文本进度条
 import time
-scale = 10
+scale = 50
 print("------执行开始-----")
 for i in range(scale+1):
     a = '■' * i
     b = '.' * (scale-i)
     c = (i/scale)*100
-    print("{:>.3f}%[{}->{}".format(c, a, b))
+    print("\r{:>.3f}%[{}{}]".format(c, a, b), end="")  # \r表示不换行
     time.sleep(0.1)
 print("------执行结束-----")
