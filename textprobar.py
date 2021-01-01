c6d56e0fa70fdc08f3208f4b391fc03b765a4130
crisyu <1021177281@qq.com>
2021-01-01 23:24:06 +0800

    单行进度条刷新不换行，且加入时间

--- textprobar.py
+++ textprobar.py
@@ -1,5 +1,5 @@
-# 文本进度条
-import time
+# 文本进度条1
+'''import time
 scale = 50
 print("------执行开始-----")
 for i in range(scale+1):
@@ -8,4 +8,19 @@ for i in range(scale+1):
     c = (i/scale)*100
     print("\r{:>.3f}%[{}{}]".format(c, a, b), end="")  # \r表示不换行
     time.sleep(0.1)
-print("------执行结束-----")
+print("------执行结束-----")'''
+
+# 文本进度条2
+import time
+scale = 50
+print("执行开始".center(scale//2, "-"))
+Start = time.perf_counter()
+for i in range(scale+1):
+    a = '■' * i
+    b = '.' * (scale-i)
+    c = (i/scale)*100
+    dur = time.perf_counter()-Start
+    print("\r{:^.3f}%[{}{}]{:.2f}s".format(
+        c, a, b, dur), end="")  # \r   end=""  表示不换行
+    time.sleep(0.1)
+print("\n"+"执行结束".center(scale//2, "-"))  # \n表示另起一行
