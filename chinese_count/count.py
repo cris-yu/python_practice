c230b2f4bb429f66321ce6b97df8a2af42bec7bb
crisyu <1021177281@qq.com>
2021-01-10 00:52:03 +0800

    人名统计

--- chinese_count/count.py
+++ chinese_count/count.py
@@ -1,4 +1,5 @@
-import jieba
+# 词频统计
+'''import jieba
 txt = open("chinese_count/threekingdoms.txt", "r", encoding='utf-8').read()
 words = jieba.lcut(txt)
 counts = {}
@@ -11,4 +12,32 @@ items = list(counts.items())
 items.sort(key=lambda x: x[1], reverse=True)
 for i in range(15):
     word, count = items[i]
-    print("{0:<10}{1:>5}".format(word, count))
+    print("{0:<10}{1:>5}".format(word, count))'''
+
+# 人名统计
+import jieba
+txt = open("chinese_count/threekingdoms.txt", "r", encoding='utf-8').read()
+excludes = {"将军", "却说", "二人", "不可", "荆州", "不能", "如此"}
+words = jieba.lcut(txt)
+counts = {}
+for word in words:
+    if len(word) == 1:
+        continue
+    elif word == "诸葛亮" or word == "孔明曰":
+        rword = "孔明"
+    elif word == "关公" or word == "云长":
+        rword = "关羽"
+    elif word == "玄德" or word == "玄德曰":
+        rword = "刘备"
+    elif word == "孟德" or word == "丞相":
+        rword = "曹操"
+    else:
+        rword = word
+    counts[rword] = counts.get(rword, 0)+1
+for word in excludes:
+    del counts[word]
+items = list(counts.items())
+items.sort(key=lambda x: x[1], reverse=True)
+for i in range(5):
+    word, count = items[i]
+    print("{0:<5}{1:>5}".format(word, count))
