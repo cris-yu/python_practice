9bb51f6bf0557e8ec390c0e8b046cfbe31feab32
crisyu <1021177281@qq.com>
2021-01-10 20:27:31 +0800

    自动轨迹绘制

---
+++ autodraw/autotracedraw.py
@@ -0,0 +1,20 @@
+import turtle as t
+t.title('自动绘制轨迹')
+t.setup(800, 600, 0, 0)
+t.pencolor("red")
+t.pensize(5)
+# 数据读取
+detals = []
+f = open("autodraw/data.txt")
+for line in f:
+    line = line.replace("\n", " ")
+    detals.append(list(map(eval, line.split(","))))
+f.close()
+# 自动绘制
+for i in range(len(detals)):
+    t.pencolor(detals[i][3], detals[i][4], detals[i][5])
+    t.fd(detals[i][0])
+    if detals[i][1]:
+        t.right(detals[i][2])
+    else:
+        t.left(detals[i][2])
